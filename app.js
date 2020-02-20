436e4cc76176900867d65f9e588e20e698094afd
Kamil Ratowski <kamilratowski90@gmail.com>
2020-02-20 18:39:46 +0100

    Add middlewares with basic routes handling

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
+
+app.use('/add-product', (req, res) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text"><button type="submit">Add Product</button></form>'
+  );
+});
+
+app.use('/product', (req, res) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
+app.use('/', (req, res) => {
+  res.send('<h1>Hello!</h1>');
+});
 
 app.listen(3000);
