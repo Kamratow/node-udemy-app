79be5990305f10e2cc8f02a79f9475a64d9ba767
Kamil Ratowski <kamilratowski90@gmail.com>
2020-02-20 18:57:20 +0100

    Implemented simple body parsing with request method detection

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use('/add-product', (req, res) => {
   res.send(
-    '<form action="/product" method="POST"><input type="text"><button type="submit">Add Product</button></form>'
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
   );
 });
 
-app.use('/product', (req, res) => {
+app.post('/product', (req, res) => {
   console.log(req.body);
   res.redirect('/');
 });
