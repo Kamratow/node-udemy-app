243121e6afc1f6690b47433bf35e193d0ae3c69e
Kamil Ratowski <kamilratowski90@gmail.com>
2020-03-03 07:14:40 +0100

    Create and use pug template for shop page

--- routes/shop.js
+++ routes/shop.js
@@ -1,15 +1,16 @@
-const path = require('path');
+// const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
-const adminData = require('./admin');
+// const rootDir = require('../util/path');
+// const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  console.log('this is products array:', adminData.products);
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  // console.log('this is products array:', adminData.products);
+  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  res.render('shop');
 });
 
 module.exports = router;
