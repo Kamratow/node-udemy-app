b6b467d33f77be2298c7cf0c20dcb84ed649a7c7
Kamil Ratowski <kamilratowski90@gmail.com>
2020-03-04 07:38:38 +0100

    Add outputting products dynamically in shop pug template

--- routes/shop.js
+++ routes/shop.js
@@ -3,14 +3,15 @@
 const express = require('express');
 
 // const rootDir = require('../util/path');
-// const adminData = require('./admin');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   // console.log('this is products array:', adminData.products);
   // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-  res.render('shop');
+  const { products } = adminData;
+  res.render('shop', { prods: products, docTitle: 'Shop' });
 });
 
 module.exports = router;
