727c9bdde3d9f2c35abaa834798cff0eb2e6813a
Kamil Ratowski <kamilratowski90@gmail.com>
2020-03-05 07:00:59 +0100

    Convert rest of html templates to pug

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,16 @@
-const path = require('path');
+// const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
+// const rootDir = require('../util/path');
 
 const router = express.Router();
 
 const products = [];
 
 router.get('/add-product', (req, res) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product', { pageTitle: 'Add Product' });
 });
 
 router.post('/add-product', (req, res) => {
