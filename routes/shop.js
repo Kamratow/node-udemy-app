8bde89842085e132b11e62679bc9e789e4fef879
Kamil Ratowski <kamilratowski90@gmail.com>
2020-03-07 18:17:45 +0100

    Clean up not used templates and comments in files

--- routes/shop.js
+++ routes/shop.js
@@ -1,15 +1,10 @@
-// const path = require('path');
-
 const express = require('express');
 
-// const rootDir = require('../util/path');
 const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // console.log('this is products array:', adminData.products);
-  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
   const { products } = adminData;
   res.render('shop', {
     prods: products,
